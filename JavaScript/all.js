aacd13240a8f07d8c4250faca618e66c2e3683ec
TieString <64260240+TieString@users.noreply.github.com>
2021-01-21 11:15:26 +0800

    整理

---
+++ JavaScript/all.js
@@ -0,0 +1,119 @@
+window.onload=function(){
+    // 获取箭头
+    
+    var arrow = document.getElementById("arrow");
+    var words = document.getElementById("words");
+    var star0 = document.getElementById("star0");
+    var star1 = document.getElementById("star1");
+    var star2 = document.getElementById("star2");
+    var star3 = document.getElementById("star3");
+    var star4 = document.getElementById("star4");
+    var star5 = document.getElementById("star5");
+    var star6 = document.getElementById("star6");
+    var star7 = document.getElementById("star7");
+    var star8 = document.getElementById("star8");
+    var star9 = document.getElementById("star9");
+    arrow.onclick = function(){
+        bar1.id = "bar_1";
+        bar2.id = "bar_2";
+        bar3.id = "bar_3";
+        bar4.id = "bar_4";
+        bar5.id = "bar_5";
+        bar6.id = "bar_6";
+        bar7.id = "bar_7";
+        bar8.id = "bar_8";
+        bar9.id = "bar_9";
+        bar10.id = "bar_10";
+        bar11.id = "bar_11";
+        bar12.id = "bar_12";
+        bar13.id = "bar_13";
+        bar14.id = "bar_14";
+        bar15.id = "bar_15";
+        bar16.id = "bar_16";
+        bar17.id = "bar_17";
+        bar18.id = "bar_18";
+        bar19.id = "bar_19";
+        bar20.id = "bar_20";
+        bar21.id = "bar_21";
+        bar22.id = "bar_22";
+        bar23.id = "bar_23";
+        bar24.id = "bar_24";
+        bar25.id = "bar_25";
+        bar26.id = "bar_26";
+        bar27.id = "bar_27";
+        bar28.id = "bar_28";
+        bar29.id = "bar_29";
+        bar30.id = "bar_30";
+        bar31.id = "bar_31";
+        bar32.id = "bar_32";
+        bar33.id = "bar_33";
+        bar34.id = "bar_34";
+        bar35.id = "bar_35";
+        bar36.id = "bar_36";
+        bar37.id = "bar_37";
+        bar38.id = "bar_38";
+        bar39.id = "bar_39";
+        bar40.id = "bar_40";
+        bar41.id = "bar_41";
+        bar42.id = "bar_42";
+        bar43.id = "bar_43";
+        bar44.id = "bar_44";
+        bar45.id = "bar_45";
+        bar46.id = "bar_46";
+        // 改变文字
+        words.innerHTML = "是你。";
+        // 更改样式
+        words.id = "word";
+        // 箭头消失
+        arrow.innerHTML = "";
+        // 星星出现
+        star0.id = "star_0";
+        star1.id = "star_1";
+        star2.id = "star_2";
+        star3.id = "star_3";
+        star4.id = "star_4";
+        star5.id = "star_5";
+        star6.id = "star_6";
+        star7.id = "star_7";
+        star8.id = "star_8";
+        star9.id = "star_9";
+    };
+    var flag = 1, countdown = 5;
+    star0.onclick = function(){
+        star0.src = "./star_yellow.png";
+        // 点亮顺序正确
+        if(flag == 3){
+
+        }
+    };
+    star1.onclick = function(){
+        star1.src = "./star_yellow.png";
+        flag = 0;
+    };
+    star2.onclick = function(){
+        star2.src = "./star_yellow.png";
+        if(flag == 0){
+
+        }
+        else if(flag == 2){
+            flag++;
+        }
+    };
+    star3.onclick = function(){
+        star3.src = "./star_yellow.png";
+        flag = 0;
+    };
+    star4.onclick = function(){
+        star4.src = "./star_yellow.png";
+        flag = 0;
+    };
+    star5.onclick = function(){
+        star5.src = "./star_yellow.png";
+        if(flag == 0){
+
+        }
+        else if(flag == 1){
+            flag++;
+        }
+    };
+};
